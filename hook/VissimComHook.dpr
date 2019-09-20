82aa195dce3874c0fac79586893fd20b6392ba90
wxinixs@kld <5317912+wxinix@users.noreply.github.com>
2019-09-20 13:43:23 -0400

    Add files via upload
    VissimComHook Dll files.  It will be hooked to Vissim process space and intercepts Windows COM object creation calls.

---
+++ hook/VissimComHook.dpr
@@ -0,0 +1,50 @@
+library VissimComHook;
+
+uses
+  madCodeHook,
+  uHookFunctions in 'uHookFunctions.pas';
+
+{$R *.res}
+
+begin
+  CollectHooks;
+
+  HookAPI(
+  	'ole32.dll',
+    'CoCreateInstance',
+     @CoCreateInstanceCallback,
+     @CoCreateInstanceNext
+  );
+
+  {$IFDEF DEBUG}
+  HookAPI(
+    'ole32.dll',
+    'CoRegisterClassObject',
+    @CoRegisterClassObjectCallback,
+    @CoRegisterClassObjectNext
+  );
+  
+  HookAPI(
+    'ole32.dll',
+    'CoRevokeClassObject',
+    @CoRevokeClassObjectCallback,
+    @CoRevokeClassObjectNext
+  );
+  
+  HookAPI(
+    'ole32.dll', 
+    'CoInitializeEx',
+    @CoInitializeExCallback,
+    @CoInitializeExNext
+  );
+  
+  HookAPI(
+    'ole32.dll', 
+    'CoUninitialize',
+    @CoUninitializeCallback,
+    @CoUninitializeNext
+  );
+  {$ENDIF}
+
+  FlushHooks;
+end.
